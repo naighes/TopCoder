9ba0f03f724ff72c1bef9540aeeff73e96cfe1f8
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-03-01 20:38:11 +0100

    trying to improve 'Cross'.

--- Cross/program.go
+++ Cross/program.go
@@ -3,10 +3,16 @@ package main
 func exist(board []string) string {
 	for i := 1; i < len(board)-1; {
 		for j := 1; j < len(board[i])-1; {
-			if match(board, i, j) {
-				return "Exists"
+			h := HMatch(board, i, j)
+			if h == 0 {
+				if match(board, i, j) {
+					return "Exists"
+				} else {
+					j = j + 1
+				}
+			} else {
+				j = j + h
 			}
-			j++
 		}
 		i++
 	}
@@ -14,9 +20,19 @@ func exist(board []string) string {
 }
 
 func match(board []string, i int, j int) bool {
-	return board[i][j] == '#' &&
-		board[i][j+1] == '#' &&
-		board[i][j-1] == '#' &&
-		board[i+1][j] == '#' &&
+	return board[i+1][j] == '#' &&
 		board[i-1][j] == '#'
+}
+
+func HMatch(board []string, i int, j int) int {
+	if board[i][j+1] != '#' {
+		return 3
+	}
+	if board[i][j] != '#' {
+		return 2
+	}
+	if board[i][j-1] != '#' {
+		return 1
+	}
+	return 0
 }
